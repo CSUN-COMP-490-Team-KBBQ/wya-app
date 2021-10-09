54180b856524f32f1ea90190dc7a3a5a767a69c6
Jan Navarro <navarrojanderrick@gmail.com>
2021-10-09 06:19:43 -0700

    created barebones create event page

--- src/components/CreateEventForm/CreateEventForm.tsx
+++ src/components/CreateEventForm/CreateEventForm.tsx
@@ -4,12 +4,92 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { v4 as uuid } from 'uuid';
 
 import './CreateEventForm.css';
 
-export default function CreateEventForm(): JSX.Element {
+interface GuestListProps {
+    guests: string[];
+    updateGuests: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+export function GuestList(props: GuestListProps): JSX.Element {
+    const { guests, updateGuests } = props;
+    const inputRef = React.useRef<HTMLInputElement>();
+    const onClickHandler = () => {
+        if (inputRef.current) {
+            const newGuestName = inputRef.current.value;
+            updateGuests([...guests, newGuestName]);
+            inputRef.current.value = '';
+        }
+    };
     return (
-        <Form data-testid="CreateEventForm">
+        <div>
+            <h4>Guest List</h4>
+            <Row>
+                <ListGroup>
+                    {guests.map((guest) => (
+                        <ListGroup.Item key={uuid()}>{guest}</ListGroup.Item>
+                    ))}
+                </ListGroup>
+            </Row>
+            <Row>
+                <Col>
+                    <FloatingLabel controlId="addGuest" label="Guest Name">
+                        <Form.Control
+                            type="text"
+                            placeholder="Guest Name"
+                            ref={(node: HTMLInputElement) => {
+                                inputRef.current = node;
+                            }}
+                        />
+                    </FloatingLabel>
+                </Col>
+                <Col>
+                    <Button type="button" onClick={onClickHandler}>
+                        Add Guest
+                    </Button>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export interface CreateEventFormInterface {
+    name: string;
+    description: string;
+    startDatetime: string;
+    endDatetime: string;
+    guests: string[];
+}
+
+interface CreateEventFormProps {
+    // eslint-disable-next-line
+    setFormHook: React.Dispatch<React.SetStateAction<any>>;
+}
+
+export default function CreateEventForm(
+    props: CreateEventFormProps
+): JSX.Element {
+    const { setFormHook } = props;
+
+    const [guests, updateGuests] = React.useState<string[]>([]);
+
+    const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        // eslint-disable-next-line
+        let formValue = Object.fromEntries(formData.entries()) as any;
+        formValue = { ...formValue, guests };
+        // eslint-disable-next-line
+        console.log('USER_CREATE_EVENT', formValue);
+        if (setFormHook) setFormHook(formValue);
+    };
+
+    return (
+        <Form data-testid="CreateEventForm" onSubmit={onSubmitHandler}>
+            <input type="hidden" name="hostId" value="Host1" />
             <Row>
                 <FloatingLabel controlId="eventName" label="Event Name">
                     <Form.Control
@@ -63,7 +143,11 @@ export default function CreateEventForm(): JSX.Element {
             </Row>
 
             <Row>
-                <Button type="button">Create</Button>
+                <GuestList guests={guests} updateGuests={updateGuests} />
+            </Row>
+
+            <Row>
+                <Button type="submit">Create</Button>
             </Row>
         </Form>
     );
--- src/pages/CreateEventPage/CreateEventPage.tsx
+++ src/pages/CreateEventPage/CreateEventPage.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import CreateEventForm from '../../components/CreateEventForm/CreateEventForm';
+import CreateEventForm, {
+    CreateEventFormInterface,
+} from '../../components/CreateEventForm/CreateEventForm';
 
 import './CreateEventPage.css';
 
 export default function CreateEventPage(): JSX.Element {
+    const [form, setForm] = React.useState<CreateEventFormInterface>();
     return (
         <div>
             <h1>CreateEventPage</h1>
             <div>
-                <CreateEventForm />
+                <CreateEventForm setFormHook={setForm} />
             </div>
+            {/* Just for backend team to see the submission of the form */}
+            <pre>{JSON.stringify(form, null, 2)}</pre>
         </div>
     );
 }
