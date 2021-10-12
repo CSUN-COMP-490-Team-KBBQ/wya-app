3fbe3e15274d557ad03b875330a6f575adc5a7e4
Jan Navarro <navarrojanderrick@gmail.com>
2021-10-12 14:18:54 -0700

    fix lint error causing failed checks

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -1,14 +1,21 @@
-import { doc, setDoc, getFirestore, getDoc } from 'firebase/firestore';
+import {
+    doc,
+    setDoc,
+    getFirestore,
+    getDoc,
+    DocumentData,
+    DocumentReference,
+} from 'firebase/firestore';
 import app from './firebase';
 import EventData from '../interfaces/Event';
 
 const firestore = getFirestore(app);
 
-const getDocRef = (path: string) => {
+const getDocRef = (path: string): DocumentReference<DocumentData> => {
     return doc(firestore, path);
 };
 
-export const createEvent = (data: EventData) => {
+export const createEvent = (data: EventData): Promise<void> => {
     const eventDocRef = getDocRef(`/events/${data.eventId}`);
     return setDoc(eventDocRef, {
         ...data,
