7349b8ca4fab4ef87a358931ae7f4f0072006b66
Jan Navarro <navarrojanderrick@gmail.com>
2021-10-12 13:19:39 -0700

    added auth logic for creating and deleting users

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,31 +1,54 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
+import { json as bodyParserJSON } from 'body-parser';
 
-const app = admin.initializeApp(functions.config());
-const firestore = admin.firestore(app);
+const firebase = admin.initializeApp(functions.config());
+const firestore = admin.firestore(firebase);
+const auth = admin.auth(firebase);
+
+// Using express and exposing functions as express widget
+// https://firebase.google.com/docs/functions/http-events#using_existing_express_apps
+const app = express();
+app.use(cors());
+app.use(bodyParserJSON());
+app.post('/register', async (req, res) => {
+    const { email, password, firstName, lastName } = req.body;
+    try {
+        functions.logger.info('Creating user');
+        const user = await auth.createUser({
+            email,
+            password,
+        });
+        const { uid } = user;
+        functions.logger.info('Creating user record');
+        await firestore.doc(`/users/${uid}`).create({
+            uid,
+            email,
+            firstName,
+            lastName,
+            events: [],
+        });
+        functions.logger.info(`User ${uid} successfully created.`);
+        res.status(200).send(user.toJSON());
+    } catch (e) {
+        if (e instanceof Error) {
+            functions.logger.error(e);
+            res.status(500).send(`${e.name}: ${e.message}`);
+        }
+    }
+});
+export const api = functions.https.onRequest(app);
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((_request, response) => {
-    functions.logger.info('Hello logs!', { structuredData: true });
-    response.send('Hello from Firebase!');
-});
-
-export default helloWorld; // this is just so eslint doesnt complain. when a file has a single export it prefers default
-
-export const createUserRecord = functions.auth
-    .user()
-    .onCreate(({ uid, email }) => {
-        return firestore.doc(`/users/${uid}`).create({
-            uid,
-            email,
-            firstName: '',
-            lastName: '',
-            events: [],
-        });
-    });
-
 export const deleteUserRecord = functions.auth.user().onDelete(({ uid }) => {
-    return firestore.doc(`/users/${uid}`).delete();
+    return firestore
+        .doc(`/users/${uid}`)
+        .delete()
+        .then(() => {
+            functions.logger.info(`User ${uid} successfully deleted.`);
+        });
 });
