6a9c68645e26ea34e7243832e095a6aff4ddb61f
Ivan Cuadra <ivan.cuadra.869@my.csun.edu>
2021-10-11 23:42:16 -0700

    add createEvent function
    The hostId is hard-coded in CreateEventForm.tsx and the eventId is auto generated by the front-end.

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -1,34 +1,23 @@
-import {
-    collection,
-    doc,
-    getDocs,
-    setDoc,
-    addDoc,
-    getFirestore,
-} from 'firebase/firestore';
+import { doc, setDoc, getFirestore } from 'firebase/firestore';
 import app from './firebase';
 
 const firestore = getFirestore(app);
 
-const eventsCollectionRef = collection(firestore, 'events');
+// createEvent makes use setDoc which will use the hostId and eventId from the front-end
+const createEvent = async (eventData: any): Promise<void> => {
+    const data = {
+        availabilities: {},
+        description: eventData.description,
+        endDate: new Date(eventData.endDatetime),
+        guests: eventData.guests,
+        hostId: eventData.hostId,
+        startDate: new Date(eventData.startDatetime),
+        title: eventData.name,
+    };
 
-// addDoc will automatically assign a unique document ID
-const addEvent = async (eventData: JSON): Promise<void> => {
-    await addDoc(eventsCollectionRef, eventData);
+    const id = eventData.eventId;
+    const eventsDocumentRef = doc(firestore, 'events', id);
+    await setDoc(eventsDocumentRef, data);
 };
 
-// setDoc will use the referenced document ID, if the document ID does not exist, it will create it.
-const setEvent = async (eventData: JSON, eventRef: string): Promise<void> => {
-    const eventsDocumentRef = doc(firestore, 'events', eventRef);
-    await setDoc(eventsDocumentRef, eventData);
-};
-
-const getEvents = async (): Promise<void> => {
-    const snapshot = await getDocs(eventsCollectionRef);
-
-    snapshot.forEach((docu) => {
-        console.log(docu.id, '=>', docu.data());
-    });
-};
-
-export default { addEvent, setEvent, getEvents };
+export default { createEvent };
