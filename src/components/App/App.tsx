951b6c2d69bad221b2ea3339309adca5a0e48293
Jan Navarro <navarrojanderrick@gmail.com>
2021-10-12 01:26:08 -0700

    added User auth contexts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,19 +6,26 @@ import LoginPage from '../../pages/LoginPage/LoginPage';
 import RegisterPage from '../../pages/RegisterPage/RegisterPage';
 import CreateEventPage from '../../pages/CreateEventPage/CreateEventPage';
 import EventPage from '../../pages/EventPage/EventPage';
+import { UserAuthProvider } from '../../contexts/UserContext';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App(): JSX.Element {
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/login" exact component={LoginPage} />
-                <Route path="/register" exact component={RegisterPage} />
-                <Route path="/create-event" exact component={CreateEventPage} />
-                <Route path="/event/:id" exact component={EventPage} />
-            </Switch>
-        </Router>
+        <UserAuthProvider>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/login" exact component={LoginPage} />
+                    <Route path="/register" exact component={RegisterPage} />
+                    <Route
+                        path="/create-event"
+                        exact
+                        component={CreateEventPage}
+                    />
+                    <Route path="/event/:id" exact component={EventPage} />
+                </Switch>
+            </Router>
+        </UserAuthProvider>
     );
 }
