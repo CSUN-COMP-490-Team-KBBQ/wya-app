cdd962042e8a2e955c844a08d2b3a1d1de46be28
Jan Navarro <navarrojanderrick@gmail.com>
2021-10-13 04:34:47 -0700

    initialized firebase emulators

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-const firebase = admin.initializeApp(functions.config());
+const firebase = admin.initializeApp();
 const firestore = admin.firestore(firebase);
 const auth = admin.auth(firebase);
 
