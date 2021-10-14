f4c073ff2091f98b7fee25d04fb9eb2b82bfd97d
Ivan Cuadra <ivan.cuadra.869@my.csun.edu>
2021-10-14 14:14:05 -0700

    Add main menu to the HomePage with basic info.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,20 +4,31 @@ import Button from 'react-bootstrap/Button';
 import { useUserContext } from '../../contexts/UserContext';
 import { logOut } from '../../lib/auth';
 
+import './HomePage.css';
+
 export default function HomePage(): JSX.Element {
     const user = useUserContext();
     return (
         <div>
-            <h1>Home Page</h1>
             {user ? (
                 <div>
-                    <pre>{JSON.stringify(user, null, 2)}</pre>
-                    <Button variant="danger" onClick={logOut}>
-                        Log out
-                    </Button>
+                    <header>
+                        <Button variant="light" onClick={logOut}>
+                            Log out
+                        </Button>
+                    </header>
+                    <h1>Main Menu</h1>
+                    <pre>Welcome: {JSON.stringify(user.email, null, 2)}</pre>
+                    <Link to="/create-event" className="btn btn-info">
+                        Create Event
+                    </Link>
+                    <Button variant="info" className="btn">
+                        Current Calander
+                    </Button>{' '}
                 </div>
             ) : (
                 <div>
+                    <h1>Home Page</h1>
                     Welcome! <Link to="/login">Login</Link> to get started.
                 </div>
             )}
