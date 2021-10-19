4b032deb6b9306db3d5f6b85ff75e1cdf43f1da0
Jorge Escobar <jorge.escobar0419@gmail.com>
2021-10-18 21:34:33 -0700

    update AvailabilityMap test to include a check for heatmap rendering

--- src/components/AvailabilityMap/AvailabilityMap.spec.tsx
+++ src/components/AvailabilityMap/AvailabilityMap.spec.tsx
@@ -3,6 +3,11 @@ import { render } from '@testing-library/react';
 import AvailabilityMap from './AvailabilityMap';
 
 it('renders component', () => {
-    const { queryByText } = render(<AvailabilityMap />);
-    expect(queryByText('AvailabilityMapComponent')).toBeTruthy();
+    const { queryByText } = render(
+        <AvailabilityMap
+            days={['Monday', 'Tuesday']}
+            handleClicks={() => undefined}
+        />
+    );
+    expect(queryByText('Monday')).toBeTruthy();
 });
