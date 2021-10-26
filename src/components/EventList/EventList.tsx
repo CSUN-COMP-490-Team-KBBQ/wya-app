8b424da1cce640198ce51e8dec291b9dafd399a8
Jorge Escobar <jorge.escobar0419@gmail.com>
2021-10-26 16:59:19 -0700

    feat: sort user event list by start date

--- src/components/EventList/EventList.tsx
+++ src/components/EventList/EventList.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
 import './EventList.css';
 import { ListGroup } from 'react-bootstrap';
+import { EventInfo } from '../../interfaces/User';
 
 interface EventListProps {
     elementId: string;
-    events: string[];
+    events: EventInfo[];
+}
+
+function sortEvents(events: EventInfo[]): EventInfo[] {
+    return Object.values(events).sort((a, b) => {
+        let result = 0;
+        const aStart = Date.parse(a.startDate);
+        const bStart = Date.parse(b.startDate);
+
+        if (aStart < bStart) {
+            result = -1;
+        } else if (aStart > bStart) {
+            result = 1;
+        }
+
+        return result;
+    });
 }
 
 export default function EventList(props: EventListProps): JSX.Element {
     const { elementId, events } = props;
+    const eventList = sortEvents(events);
 
     return (
         <div id={elementId}>
             <h1>Events</h1>
             <ListGroup className="event-list">
-                {/* 
-                    subject to change:
-                    populating list depends on current events' data structure: map {id: name} 
-                */}
-                {Object.entries(events).map(([id, name]) => {
+                {eventList.map(({ eventId, name }) => {
                     return (
-                        <ListGroup.Item key={id} action href={`/event/${id}`}>
+                        <ListGroup.Item
+                            key={eventId}
+                            action
+                            href={`/event/${eventId}`}
+                        >
                             {name}
                         </ListGroup.Item>
                     );
