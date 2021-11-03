4cebd8d7bb1a2a321b0d114bfc49e5b09eb636f5
Jorge Escobar <jorge.escobar0419@gmail.com>
2021-11-02 17:51:26 -0700

    feat: make /create-event route private

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import HomePage from '../../pages/HomePage/HomePage';
 import LoginPage from '../../pages/LoginPage/LoginPage';
 import RegisterPage from '../../pages/RegisterPage/RegisterPage';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
 import CreateEventPage from '../../pages/CreateEventPage/CreateEventPage';
 import EventPage from '../../pages/EventPage/EventPage';
 import CalendarPage from '../../pages/CalendarPage/CalendarPage';
@@ -19,9 +20,8 @@ export default function App(): JSX.Element {
                     <Route path="/" exact component={HomePage} />
                     <Route path="/login" exact component={LoginPage} />
                     <Route path="/register" exact component={RegisterPage} />
-                    <Route
+                    <PrivateRoute
                         path="/create-event"
-                        exact
                         component={CreateEventPage}
                     />
                     <Route path="/event/:id" exact component={EventPage} />
