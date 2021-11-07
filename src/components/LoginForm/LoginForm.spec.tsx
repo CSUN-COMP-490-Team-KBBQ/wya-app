8026b92f1d75b186834a4b4a914d04e96cb385e8
Jorge Escobar <jorge.escobar0419@gmail.com>
2021-11-06 17:14:40 -0700

    test: update login form test to include BrowserRouter for Link component

--- src/components/LoginForm/LoginForm.spec.tsx
+++ src/components/LoginForm/LoginForm.spec.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import LoginForm from './LoginForm';
 
 jest.mock('firebase/app');
 jest.mock('firebase/auth');
 
 it('renders component', () => {
-    const { queryAllByText } = render(<LoginForm />);
+    const { queryAllByText } = render(
+        <BrowserRouter>
+            <LoginForm />
+        </BrowserRouter>
+    );
     expect(queryAllByText('Email')).toBeTruthy();
     expect(queryAllByText('Password')).toBeTruthy();
 });
