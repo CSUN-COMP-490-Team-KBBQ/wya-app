9a4cdd4275b2395600f1358517d8527cdaf7402f
Jorge Escobar <jorge.escobar0419@gmail.com>
2021-11-07 14:35:27 -0800

    test: update password reset page test to include mocked auth

--- src/pages/PasswordResetPage/PasswordResetPage.spec.tsx
+++ src/pages/PasswordResetPage/PasswordResetPage.spec.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import PasswordResetPage from './PasswordResetPage';
 
+jest.mock('firebase/auth');
+
 it('renders component', () => {
     const { queryByText } = render(<PasswordResetPage />);
     expect(queryByText('Password Reset')).toBeTruthy();
