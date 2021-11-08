56a1a92c19304997c8cb22d37dc87b26fb7e3712
Jorge Escobar <jorge.escobar0419@gmail.com>
2021-11-07 16:39:36 -0800

    feat: add form in profile page to update user password

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './ProfilePage.css';
 
 export default function ProfilePage(): JSX.Element {
-    return <h1>ProfilePage</h1>;
+    return (
+        <div id="profilePage">
+            <h1>ProfilePage</h1>
+            <Form
+                onSubmit={() => console.log('update')}
+                className="change-password-form"
+            >
+                <h2>Change Password</h2>
+                <hr />
+                <Form.Group controlId="updatePasswordForm">
+                    <Form.Label>New Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        className="form-input"
+                    />
+                    <Button id="passwordUpdateBtn" type="submit">
+                        Update Password
+                    </Button>
+                </Form.Group>
+            </Form>
+        </div>
+    );
 }
