f6a6ff8d83df1b16a51893458660c2ab9fe29143
MarMar Tahery <mtahery20@hotmail.com>
2021-11-13 14:28:02 -0800

    draft pr for recaptcha

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useHistory } from 'react-router-dom';
-
+import ReCAPTCHA from 'react-google-recaptcha';
 import './RegisterForm.css';
 import RegisterFormData from '../../interfaces/RegisterFormData';
 import { registerUser, logIn } from '../../lib/auth';
 import { useUserContext } from '../../contexts/UserContext';
 
-export default function RegisterForm(): JSX.Element {
+export default async function RegisterForm(): Promise<JSX.Element> {
     const history = useHistory();
     const user = useUserContext();
+    const reRef = useRef<ReCAPTCHA>();
     React.useEffect(() => {
         if (user) {
             // eslint-disable-next-line
@@ -20,6 +21,13 @@ export default function RegisterForm(): JSX.Element {
             history.push('/');
         }
     });
+
+    <ReCAPTCHA
+        sitekey={process.env.RECAPTCHA_SITE_KEY}
+        size="invisible"
+        ref={reRef}
+    />;
+
     const registerHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const formData = new FormData(e.target as HTMLFormElement);
@@ -39,6 +47,10 @@ export default function RegisterForm(): JSX.Element {
             .catch(console.error);
     };
 
+    const token = await reRef.current.executeAsync();
+
+    console.log(token, 'token');
+
     return (
         <Form onSubmit={registerHandler} className="register-form">
             <Row>
@@ -52,21 +64,19 @@ export default function RegisterForm(): JSX.Element {
                     <Form.Control type="text" name="lastName" />
                 </Form.Group>
             </Row>
-
             <Row>
                 <Form.Group controlId="registerEmail">
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" name="email" />
                 </Form.Group>
             </Row>
-
             <Row>
                 <Form.Group controlId="registerPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" name="password" />
                 </Form.Group>
             </Row>
-
+            token
             <Row>
                 <Form.Group>
                     <Button type="submit">Register</Button>
