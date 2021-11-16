ba50a21f6368f7fb7ebd8e1fce9b72e4c24806dc
Jan Navarro <navarrojanderrick@gmail.com>
2021-11-16 15:39:11 -0800

    refactored UserRecordContext and added an internal pending state

--- src/contexts/UserRecordContext.tsx
+++ src/contexts/UserRecordContext.tsx
@@ -3,24 +3,37 @@ import UserData from '../interfaces/User';
 import { getDocSnapshot$ } from '../lib/firestore';
 import { useUserContext } from './UserContext';
 
-const UserRecordContext = React.createContext<UserData | null>(null);
+type UserRecordState = {
+    pending: boolean;
+    userRecord: UserData | null;
+};
+
+const UserRecordContext = React.createContext<UserRecordState>({
+    pending: true,
+    userRecord: null,
+});
 
 export const UserRecordProvider: React.FC = ({ children }) => {
-    const [userRecord, setUserRecord] = React.useState<UserData | null>(null);
+    const [userRecord, setUserRecord] = React.useState<UserRecordState>({
+        pending: true,
+        userRecord: null,
+    });
 
-    const user = useUserContext();
+    const { pending, user } = useUserContext();
 
     React.useEffect(() => {
-        if (user) {
+        if (!pending && user) {
             return getDocSnapshot$(`/users/${user.uid}`, {
                 next: (snapshot) => {
                     if (snapshot.exists()) {
-                        setUserRecord(snapshot.data() as UserData);
+                        setUserRecord({
+                            pending: false,
+                            userRecord: snapshot.data() as UserData,
+                        });
                     }
                 },
             });
         }
-        return setUserRecord(null);
     }, [user]);
 
     return (
@@ -30,7 +43,7 @@ export const UserRecordProvider: React.FC = ({ children }) => {
     );
 };
 
-export const useUserRecordContext = (): UserData | null => {
+export const useUserRecordContext = (): UserRecordState => {
     const userRecord = React.useContext(UserRecordContext);
     if (userRecord === undefined) {
         throw new Error(
