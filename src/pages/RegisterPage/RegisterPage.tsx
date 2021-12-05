8cbaec780df7318727ba7fc182be30c04a3ea2a2
Jan Navarro <navarrojanderrick@gmail.com>
2021-12-05 07:12:12 -0800

    Use Container in RegisterPage and move link to RegisterForm

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import finallogo from '../../assets/wya test 4.png';
 import './RegisterPage.css';
 
 export default function RegisterPage(): JSX.Element {
     return (
-        <div>
+        <Container fluid>
             <img src={finallogo} alt="logo" />
             <h1>Register</h1>
-            <div className="form-container">
-                <RegisterForm />
-                <div className="form-footer">
-                    Have an account? Log in&nbsp;
-                    <Link to="/login">here</Link>
-                </div>
-            </div>
-        </div>
+            <RegisterForm />
+        </Container>
     );
 }
