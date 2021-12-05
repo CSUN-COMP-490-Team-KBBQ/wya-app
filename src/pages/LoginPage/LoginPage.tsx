15412f4d2c8833097bd5a89e3744f1f90f49579f
Jan Navarro <navarrojanderrick@gmail.com>
2021-12-05 06:36:28 -0800

    Utilize bootstrap Container in LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import finallogo from '../../assets/wya test 4.png';
 import './LoginPage.css';
 
 export default function LoginPage(): JSX.Element {
     return (
-        <div>
+        <Container fluid>
             <img src={finallogo} alt="logo" />
             <h1>Log In</h1>
-            <div className="form-container">
-                <LoginForm />
-                <div className="form-footer">
-                    Don&apos;t have an account? Register&nbsp;
-                    <Link to="/register">here</Link>
-                </div>
-            </div>
-        </div>
+            <LoginForm />
+        </Container>
     );
 }
