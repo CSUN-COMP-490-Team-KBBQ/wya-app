da03a05def45ee8f5ebc93d3590bc21739c83775
Jan Navarro <navarrojanderrick@gmail.com>
2021-12-05 20:46:00 -0800

    Add logo and use Container in CreateEventPage

--- src/pages/CreateEventPage/CreateEventPage.tsx
+++ src/pages/CreateEventPage/CreateEventPage.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import CreateEventForm from '../../components/CreateEventForm/CreateEventForm';
-
+import finallogo from '../../assets/wya test 4.png';
 import './CreateEventPage.css';
 
 export default function CreateEventPage(): JSX.Element {
     return (
-        <div>
-            <h1>CreateEventPage</h1>
-            <div>
-                <CreateEventForm />
-            </div>
-        </div>
+        <Container fluid>
+            <img src={finallogo} alt="logo" />
+            <h1>Create Event</h1>
+            <CreateEventForm />
+        </Container>
     );
 }
