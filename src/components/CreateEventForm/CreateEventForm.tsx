7ced9e5aaca0a9586ed4abb8be958b5e2724f0b8
Jan Navarro <navarrojanderrick@gmail.com>
2021-12-11 15:49:27 -0800

    Move create button to end of right column

--- src/components/CreateEventForm/CreateEventForm.tsx
+++ src/components/CreateEventForm/CreateEventForm.tsx
@@ -165,11 +165,13 @@ export default function CreateEventForm(): JSX.Element {
                                     updateGuests={updateGuests}
                                 />
                             </Row>
+                            <div className="button-container">
+                                <Button type="submit" className="form-button">
+                                    Create
+                                </Button>
+                            </div>
                         </Col>
                     </Row>
-                    <Button type="submit" className="form-button">
-                        Create
-                    </Button>
 
                     <Recaptcha recaptchaRef={recaptchaRef} />
                 </Form>
