f139161d8f494d0ab352b346ecaa05ec7a95d391
Jan Navarro <navarrojanderrick@gmail.com>
2021-12-11 19:07:22 -0800

    Use Container in NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+<<<<<<< HEAD
 import Page from '../../components/Page/Page';
+=======
+import Container from 'react-bootstrap/Container';
+>>>>>>> edb7403 (Use Container in NotFoundPage)
 import './NotFoundPage.css';
 
 export default function NotFoundPage(): JSX.Element {
     return (
+<<<<<<< HEAD
         <Page>
             <div className="not-found-page">
                 <h1>404 Page Not Found</h1>
             </div>
         </Page>
+=======
+        <Container>
+            <h1
+                style={{
+                    margin: 'auto',
+                    marginTop: '250px',
+                }}
+            >
+                404 Page Not Found
+            </h1>
+        </Container>
+>>>>>>> edb7403 (Use Container in NotFoundPage)
     );
 }
