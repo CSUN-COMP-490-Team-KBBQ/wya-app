a1a3ad01533a058ade3199ab02dcc62fe6bd0ddd
Jan Navarro <navarrojanderrick@gmail.com>
2021-12-11 19:25:24 -0800

    Use Container in ProfilePage

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Alert from 'react-bootstrap/Alert';
 import Toggle from 'react-toggle';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 import ChangePasswordForm from '../../components/ChangePasswordForm/ChangePasswordForm';
 import Page from '../../components/Page/Page';
@@ -102,22 +104,26 @@ export default function ProfilePage(): JSX.Element {
 
     return (
         <Page>
-            <div id="profilePage">
-                <h1>ProfilePage</h1>
-                <Form
-                    onSubmit={onSubmitHandler}
-                    className="change-password-form"
-                >
-                    <h2>Change Password</h2>
-                    <hr />
-                    <DisplayPasswordChangeForm />
-                </Form>
-                <Form className="change-display-form">
-                    <h2>Display</h2>
-                    <hr />
-                    <ShowChangeDisplayForm />
-                </Form>
-            </div>
+            <Container>
+                <Row>
+                    <h1>ProfilePage</h1>
+                </Row>
+                <Row>
+                    <Form
+                        onSubmit={onSubmitHandler}
+                        className="change-password-form"
+                    >
+                        <h2>Change Password</h2>
+                        <hr />
+                        <DisplayPasswordChangeForm />
+                    </Form>
+                    <Form className="change-display-form">
+                        <h2>Display</h2>
+                        <hr />
+                        <ShowChangeDisplayForm />
+                    </Form>
+                </Row>
+            </Container>
         </Page>
     );
 }
