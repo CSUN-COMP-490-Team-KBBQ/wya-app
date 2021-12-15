02a0b2ce4b0cc46dc1c12d169ff8c2048c1318cf
Ivan Cuadra <ivan.cuadra.869@my.csun.edu>
2021-12-15 10:41:13 -0800

    styling: change event-list border, width, and height; and page width

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
-<<<<<<< HEAD
-import Page from '../../components/Page/Page';
-=======
 import Container from 'react-bootstrap/Container';
->>>>>>> edb7403 (Use Container in NotFoundPage)
+import Page from '../../components/Page/Page';
 import './NotFoundPage.css';
 
 export default function NotFoundPage(): JSX.Element {
     return (
-<<<<<<< HEAD
         <Page>
-            <div className="not-found-page">
-                <h1>404 Page Not Found</h1>
-            </div>
+            <Container>
+                <h1
+                    style={{
+                        margin: 'auto',
+                        marginTop: '250px',
+                    }}
+                >
+                    404 Page Not Found
+                </h1>
+            </Container>
         </Page>
-=======
-        <Container>
-            <h1
-                style={{
-                    margin: 'auto',
-                    marginTop: '250px',
-                }}
-            >
-                404 Page Not Found
-            </h1>
-        </Container>
->>>>>>> edb7403 (Use Container in NotFoundPage)
     );
 }
