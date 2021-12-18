b4ab2083ee6baac054ed10e148674190003e43c3
Jorge Escobar <jorge.escobar0419@gmail.com>
2021-12-18 15:47:03 -0800

    refactor: remove nav hearder from landing page and wrap center text around logo
    Signed-off-by: Jorge Escobar <jorge.escobar0419@gmail.com>

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './HomePage.css';
 import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
 import { useUserRecordContext } from '../../contexts/UserRecordContext';
 import PageSpinner from '../../components/PageSpinner/PageSpinner';
 import Page from '../../components/Page/Page';
+import logo from '../../assets/wya-logo.png';
 
 export default function HomePage(): JSX.Element {
     const { pending, userRecord } = useUserRecordContext();
@@ -45,29 +47,34 @@ export default function HomePage(): JSX.Element {
     }
 
     return (
-        <Page>
-            <div className="home-page-content">
-                <h1>
-                    Welcome!{' '}
-                    <Link to="/login" className="loginLink">
-                        Login
-                    </Link>{' '}
-                    to get started.{' '}
-                </h1>
-                <h2>
-                    wya? is a web-based application that will help you, your
-                    friends, or your family meet up without the headache of
-                    planning. We are creating a system to easily make plans by
-                    stacking personal schedules so that your group will be able
-                    to clearly see who&apos;s doing what and when. The
-                    add-a-friend feature and overall simplicity of wya? removes
-                    the need for multiple apps and lets you chat, plan, and
-                    invite whomever you&apos;d like.
-                </h2>
-                <h1>
-                    Our app removes all the stress when planning all the fun!
-                </h1>
-            </div>
-        </Page>
+        <div className="home-page-content">
+            <h1>
+                Welcome!{' '}
+                <Link to="/login" className="loginLink">
+                    Login
+                </Link>{' '}
+                to get started.{' '}
+            </h1>
+            <Image
+                src={logo}
+                fluid
+                style={{
+                    height: '200px',
+                    float: 'right',
+                    marginRight: '100px',
+                }}
+            />
+            <h2>
+                wya? is a web-based application that will help you, your
+                friends, or your family meet up without the headache of
+                planning. We are creating a system to easily make plans by
+                stacking personal schedules so that your group will be able to
+                clearly see who&apos;s doing what and when. The add-a-friend
+                feature and overall simplicity of wya? removes the need for
+                multiple apps and lets you chat, plan, and invite whomever
+                you&apos;d like.
+            </h2>
+            <h1>Our app removes all the stress when planning all the fun!</h1>
+        </div>
     );
 }
